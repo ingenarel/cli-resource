85274dd2102a85450d20d4e2a2b0cf86f5fc48d4
ingenarel <ingenarelitems@gmail.com>
2024-06-11 07:15:51 +0000

    Jun 11, 2024, 1:15 PM

--- project.py
+++ project.py
@@ -184,9 +184,6 @@ def readwrite(section:str, key:str, value, data_type:type=str, section_comment:s
             x = config[section][key][1:-1] if re.search(r"^\".+\"$", config[section][key]) else config[section][key]
             return data_type(x)
         except KeyError:
-            # config[section]={
-            #     key: f"\"{value}\"" if data_type==str else value,
-            # }
             if config.has_section(section):
                 config.read(config_file_name)
             else:
