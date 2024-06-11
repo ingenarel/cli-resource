c3adccee68a426faaaa6e2ce97be77592c4bfc8d
ingenarel <ingenarelitems@gmail.com>
2024-06-11 11:52:42 +0000

    Jun 11, 2024, 5:52 PM

--- project.py
+++ project.py
@@ -13,7 +13,6 @@ except ModuleNotFoundError:
 
 try:
     from colorama import Fore, init
-
     init(autoreset=True)
 except ModuleNotFoundError:
     print(f"You're missing the colorama module.")
@@ -23,17 +22,14 @@ except ModuleNotFoundError:
 osname = os_name
 
 if osname == "nt":
-
     def clear():
         os_system("cls")
 
 elif osname == "posix":
-
     def clear():
         os_system("clear")
 
-
-def bar_maker_ini(bar_height: int = 10, box: str = "█"):
+def bar_maker_ini(bar_height:int=10, box:str="█"):
     """
     This is the initialization before the bar_maker() function makes the bars.
     It takes these value:
@@ -57,119 +53,88 @@ def bar_maker_ini(bar_height: int = 10, box: str = "█"):
         exit("The bar height should be an int.")
     elif bar_height < 3:
         exit("The bar height is too low. it should at least be 3")
-    bar_box_number_list = [bar_number + 1 for bar_number in range(bar_height)]
-    g = int(bar_height * 0.429)
-    y = int(bar_height * 0.286)
-    r = int(bar_height * 0.143)
+    bar_box_number_list = [bar_number+1 for bar_number in range(bar_height)]
+    g = int(bar_height*0.429)
+    y = int(bar_height*0.286)
+    r = int(bar_height*0.143)
 
-    if g + y + r < bar_height:
+    if g+y+r < bar_height:
         g += 1
-        if g + y + r < bar_height:
+        if g+y+r < bar_height:
             y += 1
-            if g + y + r < bar_height:
+            if g+y+r < bar_height:
                 r += 1
 
     first_items_that_are_green = bar_box_number_list[:g]
-    first_items_that_are_yellow = bar_box_number_list[g : g + y]
-    first_items_that_are_red = bar_box_number_list[g + y : g + y + r]
-    bar_height_and_color = {
-        green_boxes: Fore.GREEN + box for green_boxes in first_items_that_are_green
-    }
-    bar_height_and_color.update(
-        {
-            yellow_boxes: Fore.YELLOW + box
-            for yellow_boxes in first_items_that_are_yellow
-        }
-    )
-    bar_height_and_color.update(
-        {red_boxes: Fore.RED + box for red_boxes in first_items_that_are_red}
-    )
+    first_items_that_are_yellow = bar_box_number_list[g:g+y]
+    first_items_that_are_red = bar_box_number_list[g+y:g+y+r]
+    bar_height_and_color = {green_boxes:Fore.GREEN+box for green_boxes in first_items_that_are_green}
+    bar_height_and_color.update({yellow_boxes:Fore.YELLOW+box for yellow_boxes in first_items_that_are_yellow})
+    bar_height_and_color.update({red_boxes:Fore.RED+box for red_boxes in first_items_that_are_red})
 
     return bar_height_and_color
 
-
-def bar_maker(
-    resource_usage: int,
-    bar_height_and_color: dict,
-    bar_width: int,
-    fill: str = ".",
-    zero_fill: str = ".",
-):
+def bar_maker(resource_usage:int, bar_height_and_color:dict, bar_width:int, fill:str=".", zero_fill:str="."):
     """
     this is the bar maker function. it takes these values:
     resource_usage which should be an int
 
     """
     return [
-        (
-            bar_height_and_color[resource_value]
-            if resource_value <= resource_usage
-            else zero_fill * bar_width if resource_value == 1 else fill * bar_width
-        )
-        for resource_value in bar_height_and_color
-    ]
-
+        bar_height_and_color[resource_value] if resource_value <= resource_usage
+        else
+            zero_fill*bar_width if resource_value == 1
+            else fill*bar_width
+        for resource_value in bar_height_and_color]
 
 def chart_maker(
-    last_bar: list,
-    last_chart: list,
-):
+        last_bar:list,
+        last_chart:list,
+        ):
     last_chart.pop(0)
     last_chart.append(last_bar)
     return last_chart
 
-
 def chart_parser(
-    chart: list,
-    bar_width: int,
-    chart_width: int = 30,
-    chart_name: str = None,
-    left_gap: int = 0,
-    right_gap: int = 0,
-    resource_usage: str = None,
-    left_side: str = "|",
-    right_side: str = "|",
-    roof: str = "_",
-    floor: str = "‾",
-    name_seperator: str = "=",
-    value_seperator: str = "=",
-):
-    full_width = (
-        chart_width * bar_width
-        + len(left_side)
-        + len(right_side)
-        + left_gap
-        + right_gap
-    )
-    proper_chart = [roof * int(full_width / len(roof))]
-    x = chart_width * bar_width + left_gap + right_gap
+    chart:list,
+    bar_width:int,
+    chart_width:int=30,
+    chart_name:str=None,
+    left_gap:int=0,
+    right_gap:int=0,
+    resource_usage:str=None,
+    left_side:str="|",
+    right_side:str="|",
+    roof:str="_",
+    floor:str="‾",
+    name_seperator:str="=",
+    value_seperator:str="="
+        ):
+    full_width = chart_width*bar_width+len(left_side)+len(right_side)+left_gap+right_gap
+    proper_chart = [roof*int(full_width/len(roof))]
+    x = chart_width*bar_width+left_gap+right_gap
 
     if chart_name != None:
         proper_chart.append(f"{left_side}{chart_name:^{x}}{right_side}")
-        proper_chart.append(
-            left_side + name_seperator * int(x / len(name_seperator)) + right_side
-        )
+        proper_chart.append(left_side+name_seperator*int(x/len(name_seperator))+right_side)
 
     for shit in [list(row) for row in zip(*chart)][::-1]:
         y = left_side
-        y += " " * left_gap
+        y += " "*left_gap
         for stuff in shit:
             y += stuff
-        y += " " * right_gap
+        y += " "*right_gap
         y += right_side
         proper_chart.append(y)
 
     if resource_usage != None:
-        proper_chart.append(
-            left_side + value_seperator * int(x / len(value_seperator)) + right_side
-        )
-        resource_usage = resource_usage + "%"
+        proper_chart.append(left_side+value_seperator*int(x/len(value_seperator))+right_side)
+        resource_usage = resource_usage+"%"
         proper_chart.append(f"{left_side}{resource_usage:^{x}}{right_side}")
 
-    proper_chart.append(floor * int(full_width / len(floor)))
+    proper_chart.append(floor*int(full_width/len(floor)))
 
     return proper_chart
-
 
 def av_cpu_ini():
     cpu_bar_height = readwrite("CPU", "cpu_bar_height", 10, int)
@@ -187,19 +152,11 @@ def av_cpu_ini():
     cpu_name_seperator = readwrite("CPU", "cpu_name_seperator", "=")
     cpu_value_seperator = readwrite("CPU", "cpu_value_seperator", "=")
 
-    cpu_bar_width = len(cpu_box)
+    cpu_bar_width= len(cpu_box)
 
-    cpu_starting_chart: list = [
-        [
-            "." * cpu_bar_width if box == 0 else cpu_fill * cpu_bar_width
-            for box in range(cpu_bar_height)
-        ]
-        for _ in range(cpu_chart_width)
-    ]
-    cpu_bar_height_and_color: dict = bar_maker_ini(
-        bar_height=cpu_bar_height, box=cpu_box
-    )
-    return [
+    cpu_starting_chart:list = [["."*cpu_bar_width if box == 0 else cpu_fill*cpu_bar_width for box in range(cpu_bar_height)]for _ in range(cpu_chart_width)]
+    cpu_bar_height_and_color:dict = bar_maker_ini(bar_height=cpu_bar_height,box=cpu_box)
+    return[
         {
             "cpu_bar_height": cpu_bar_height,
             "cpu_chart_width": cpu_chart_width,
@@ -215,39 +172,33 @@ def av_cpu_ini():
             "cpu_zero_fill": cpu_zero_fill,
             "cpu_name_seperator": cpu_name_seperator,
             "cpu_value_seperator": cpu_value_seperator,
-            "cpu_bar_width": cpu_bar_width,
+            "cpu_bar_width": cpu_bar_width
         },
         cpu_starting_chart,
         cpu_bar_height_and_color,
     ]
 
-
-def readwrite(section: str, key: str, value, data_type: type = str):
+def readwrite(section:str, key:str, value, data_type:type=str):
     config = configparser.ConfigParser()
     config_file_name = "resource_monitor.cfg"
     config.read(config_file_name)
     value = str(value)
     while True:
         try:
-            x = (
-                config[section][key][1:-1]
-                if re.search(r"^\".+\"$", config[section][key])
-                else config[section][key]
-            )
+            x = config[section][key][1:-1] if re.search(r"^\".+\"$", config[section][key]) else config[section][key]
             return data_type(x)
         except KeyError:
-            config[section] = {
-                key: f'"{value}"' if data_type == str else value,
+            config[section]={
+                key: f"\"{value}\"" if data_type==str else value,
             }
             if config.has_section(section):
                 config.read(config_file_name)
         except ValueError:
             config.read(config_file_name)
-            config[section][key] = f'"{value}"' if data_type == str else value
+            config[section][key] = f"\"{value}\"" if data_type==str else value
         with open(config_file_name, "w") as configfile:
             config.write(configfile)
         continue
-
 
 def main():
     av_cpu_values, av_cpu_starting_chart, av_cpu_bar_height_and_color = av_cpu_ini()
@@ -255,16 +206,14 @@ def main():
     while True:
         av_cpu_percent = psutil_cpu_percent()
         av_cpu_last_chart = chart_maker(
-            last_bar=bar_maker(
-                resource_usage=int(
-                    av_cpu_percent / 100 * av_cpu_values["cpu_bar_height"]
-                ),
+            last_bar = bar_maker(
+                resource_usage=int(av_cpu_percent/100*av_cpu_values["cpu_bar_height"]),
                 bar_height_and_color=av_cpu_bar_height_and_color,
                 bar_width=av_cpu_values["cpu_bar_width"],
                 fill=av_cpu_values["cpu_fill"],
-                zero_fill=av_cpu_values["cpu_zero_fill"],
+                zero_fill=av_cpu_values["cpu_zero_fill"]
             ),
-            last_chart=av_cpu_starting_chart if start == True else av_cpu_last_chart,
+            last_chart=av_cpu_starting_chart if start==True else av_cpu_last_chart
         )
         av_cpu_chart_proper = chart_parser(
             chart=av_cpu_last_chart,
@@ -284,9 +233,8 @@ def main():
         clear()
         for rows in av_cpu_chart_proper:
             print(rows)
-        start = False
+        start=False
         time_sleep(1)
-
 
 if __name__ == "__main__":
     main()
