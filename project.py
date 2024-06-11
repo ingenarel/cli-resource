3633ca8c880dcecaa0645819c9e20843b0e29ea1
ingenarel <ingenarelitems@gmail.com>
2024-06-11 12:21:25 +0600

    Update project.py

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ from os import name as os_name
 from os import system as os_system
 from time import sleep as time_sleep
 from subprocess import run as subprocess_run
+import configparser, re
 
 try:
     from psutil import cpu_percent as psutil_cpu_percent
@@ -81,19 +82,21 @@ def chart_maker(
         left_side:str="|",
         right_side:str="|",
         roof:str="_",
-        floor:str="‾"
+        floor:str="‾",
+        name_seperator:str="=",
+        value_seperator:str="="
         ):
 
     last_chart.pop(0)
     last_chart.append(last_bar)
 
     full_width = chart_width*bar_width+len(left_side)+len(right_side)+left_gap+right_gap
-    proper_chart = [roof*full_width]
+    proper_chart = [roof*int(full_width/len(roof))]
+    x = chart_width*bar_width+left_gap+right_gap
 
     if chart_name != None:
-        x = chart_width*bar_width+left_gap+right_gap
         proper_chart.append(f"{left_side}{chart_name:^{x}}{right_side}")
-        proper_chart.append(left_side+"="*x+right_side)
+        proper_chart.append(left_side+name_seperator*int(x/len(name_seperator))+right_side)
 
     for shit in [list(row) for row in zip(*last_chart)][::-1]:
         y = left_side
@@ -105,67 +108,119 @@ def chart_maker(
         proper_chart.append(y)
 
     if resource_usage != None:
-        proper_chart.append(left_side+"="*x+right_side)
+        proper_chart.append(left_side+value_seperator*int(x/len(value_seperator))+right_side)
         resource_usage = resource_usage+"%"
         proper_chart.append(f"{left_side}{resource_usage:^{x}}{right_side}")
 
-    proper_chart.append(floor*full_width)
+    proper_chart.append(floor*int(full_width/len(floor)))
 
     return proper_chart
 
-def main():
-    cpu_bar_height:int = 10
-    cpu_box:str = ";)"
-    cpu_chart_width:int = 10
+def av_cpu_ini():
+    cpu_bar_height:int = 30
+    cpu_chart_width:int = 140
     cpu_left_gap:int = 0
     cpu_right_gap:int = 0
     cpu_heading:str = "CPU Usage:"
+    cpu_box:str = "█"
     cpu_left_side:str = "|"
     cpu_right_side:str = "|"
     cpu_roof:str = "_"
     cpu_floor:str = "‾"
     cpu_fill:str = " "
     cpu_zero_fill:str = "."
+    cpu_name_seperator:str = "="
+    cpu_value_seperator:str = "="
 
     cpu_bar:dict = bar_maker_ini(
         bar_height=cpu_bar_height,
         box=cpu_box
         )
-    cpu_start:bool = True
     cpu_bar_width= len(cpu_box)
     cpu_starting_chart:list = [["."*cpu_bar_width if box == 0 else cpu_fill*cpu_bar_width for box in range(cpu_bar_height)]for _ in range(cpu_chart_width)]
+    # cpu_start:bool = True
 
     if cpu_chart_width < len(cpu_heading):
         exit("cpu chart width is less than cpu heading.")
+    cpu_start = True
+    cpu_usage = psutil_cpu_percent()
+    common = {
+            "last_bar":bar_maker(
+                resource_usage=int(cpu_usage/100*cpu_bar_height),
+                bar_height_and_color=cpu_bar,
+                bar_width=cpu_bar_width,
+                fill=cpu_fill,
+                zero_fill=cpu_zero_fill
+                ),
+            "chart_width": cpu_chart_width,
+            "chart_name": cpu_heading,
+            "left_gap": cpu_left_gap,
+            "right_gap": cpu_right_gap,
+            "resource_usage": str(cpu_usage),
+            "left_side": cpu_left_side,
+            "right_side": cpu_right_side,
+            "roof": cpu_roof,
+            "floor": cpu_floor,
+            "bar_width": cpu_bar_width,
+            "name_seperator": cpu_name_seperator,
+            "value_seperator": cpu_value_seperator,
+    }
+    if cpu_start == True:
+        last_cpu_starting_chart = chart_maker(**common, last_chart=cpu_starting_chart)
+        cpu_start = False
+    else:
+        last_cpu_starting_chart = chart_maker(**common, last_chart=last_cpu_starting_chart)
 
+    for rows in last_cpu_starting_chart:
+        yield rows
+
+def readwrite(section:str, key:str, value, data_type:type=str):
+    config = configparser.ConfigParser()
+    config_file_name = "resource_monitor.cfg"
+    config.read(config_file_name)
+    value = str(value)
     while True:
-        cpu_usage = psutil_cpu_percent()
-        common = {
-                "last_bar":bar_maker(
-                    resource_usage=int(cpu_usage/100*cpu_bar_height),
-                    bar_height_and_color=cpu_bar,
-                    bar_width=cpu_bar_width,
-                    fill=cpu_fill,
-                    zero_fill=cpu_zero_fill
-                    ),
-                "chart_width": cpu_chart_width,
-                "chart_name": cpu_heading,
-                "left_gap": cpu_left_gap,
-                "right_gap": cpu_right_gap,
-                "resource_usage": str(cpu_usage),
-                "left_side": cpu_left_side,
-                "right_side": cpu_right_side,
-                "roof": cpu_roof,
-                "floor": cpu_floor,
-                "bar_width": cpu_bar_width,
-        }
-        last_cpu_starting_chart = chart_maker(
-            **common, last_chart=cpu_starting_chart if cpu_start==True
-                                    else last_cpu_starting_chart)
-        time_sleep(1)
-        clear()
-        for rows in last_cpu_starting_chart:
-            print(rows)
+        try:
+            x = config[section][key][1:-1] if re.search(r"^\".+\"$", config[section][key]) else config[section][key]
+            return data_type(x)
+        except KeyError:
+            config[section]={
+                key: f"\"{value}\"" if data_type==str else value,
+            }
+            if config.has_section(section):
+                config.read(config_file_name)
+            with open(config_file_name, "w") as configfile:
+                config.write(configfile)
+            continue
+        except ValueError:
+            config.read(config_file_name)
+            config[section][key] = f"\"{value}\"" if data_type==str else value
+            with open(config_file_name, "w") as configfile:
+                config.write(configfile)
+            continue
+
+def startup():
+    cpu_bar_height:int = readwrite("CPU", "cpu_bar_height", 10, int)
+    cpu_chart_width:int = readwrite("CPU", "cpu_chart_width", 30, int)
+    cpu_left_gap:int = readwrite("CPU", "cpu_left_gap", 0, int)
+    cpu_right_gap:int = readwrite("CPU", "cpu_right_gap", 0, int)
+    cpu_heading:str = readwrite("CPU", "cpu_heading", "CPU Usage:")
+    cpu_box:str = readwrite("CPU", "cpu_box", "█")
+    cpu_left_side:str = readwrite("CPU", "cpu_left_side", "|")
+    cpu_right_side:str = readwrite("CPU", "cpu_right_side", "|")
+    cpu_roof:str = readwrite("CPU", "cpu_roof", "_")
+    cpu_floor:str = readwrite("CPU", "cpu_floor", "‾")
+    cpu_fill:str = readwrite("CPU", "cpu_fill", " ")
+    cpu_zero_fill:str = readwrite("CPU", "cpu_zero_fill", ".")
+    cpu_name_seperator:str = readwrite("CPU", "cpu_name_seperator", "=")
+    cpu_value_seperator:str = readwrite("CPU", "cpu_value_seperator", "=")
+
+def main():
+    startup()
+    # config = configparser.ConfigParser()
+    # config_file_name = "resource_monitor.cfg"
+    # config.read(config_file_name)
+    # print(type(config["CPU"]["cpu_bar_height"]))
 
 if __name__ == "__main__":
     main()
