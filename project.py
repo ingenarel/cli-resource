736d0d44c733bca24f60fab06c866bacef1f762d
ingenarel <ingenarelitems@gmail.com>
2024-06-11 09:27:49 +0000

    Jun 11, 2024, 3:27 PM

--- project.py
+++ project.py
@@ -191,22 +191,22 @@ def readwrite(section:str, key:str, value, data_type:type=str):
 
 def main():
     cpu_start = True
-    cpu_starting_chart, common = av_cpu_ini()
 
-    # while True:
-    #     if cpu_start == True:
-    #         last_cpu_starting_chart = chart_maker(**common, last_chart=cpu_starting_chart)
-    #         cpu_start = False
-    #     else:
-    #         last_cpu_starting_chart = chart_maker(**common, last_chart=last_cpu_starting_chart)
-    #     clear()
-    #     for rows in last_cpu_starting_chart:
-    #         print(rows)
-    #     time_sleep(1)
+    while True:
+        cpu_starting_chart, common = av_cpu_ini()
+        if cpu_start == True:
+            last_cpu_starting_chart = chart_maker(**common, last_chart=cpu_starting_chart)
+            cpu_start = False
+        else:
+            last_cpu_starting_chart = chart_maker(**common, last_chart=last_cpu_starting_chart)
+        clear()
+        for rows in last_cpu_starting_chart:
+            print(rows)
+        time_sleep(1)
 
-    # for shit in cpu_starting_chart:
-    #     print(shit)
-    print(common)
+    # # for shit in cpu_starting_chart:
+    # #     print(shit)
+    # print(common)
 
 if __name__ == "__main__":
     main()
